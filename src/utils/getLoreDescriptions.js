63f2d65d6e6a2056d76a827095849e39fe0f5749
Dylan Ho <dylan.ho@thecarousell.com>
2020-05-26 23:01:45 +0800

    Create loreDescriptions object

--- src/utils/getLoreDescriptions.js
+++ src/utils/getLoreDescriptions.js
@@ -2,38 +2,49 @@ const fs = require("fs");
 const request = require("request"); // https://stackoverflow.com/questions/5801453/in-node-js-express-how-do-i-download-a-page-and-gets-its-html/51000443
 const cheerio = require("cheerio");
 
-const CLAN_NAMES = {
-  Stag: "Eikthyrnir",
-  Goat: "Heidrun",
-  Wolf: "Fenrir",
-  Raven: "Huginn_and_Muninn",
-  Bear: "Bjarki",
-  Boar: "Slidrugtanni",
-  Snake: "Sváfnir",
-  Dragon: "Nidhogg",
-  Horse: "Svadilfari",
-  Kraken: "Lyngbakr",
-  Ox: "Himminbrjotir",
-};
+const CLANS = [
+  "Eikthyrnir",
+  "Heidrun",
+  "Fenrir",
+  "Huginn_and_Muninn",
+  "Bjarki",
+  "Slidrugtanni",
+  "Sváfnir",
+  "Nidhogg",
+  "Svadilfari",
+  "Lyngbakr",
+  "Himminbrjotir",
+];
 
-Object.entries(CLAN_NAMES).forEach(([name, fancyName]) => {
-  request(
-    { uri: `https://northgard.gamepedia.com/${fancyName}` },
-    (error, response, body) => {
-      const descriptions = [];
-      const $ = cheerio.load(body);
-      $("p.tooltip").each((i, elem) => {
-        const descriptionWithMultipleSpaces = $(elem).text();
-        const description = descriptionWithMultipleSpaces.replace(
-          /\s\s+/g,
-          " "
-        );
-        descriptions.push(description);
-      });
-      fs.writeFileSync(
-        `../descriptions/${name}.json`,
-        JSON.stringify(descriptions)
-      );
-    }
+function requestWrapper(fancyName, loreDescriptions) {
+  return new Promise((resolve, reject) => {
+    request(
+      { uri: `https://northgard.gamepedia.com/${fancyName}` },
+      (error, response, body) => {
+        const $ = cheerio.load(body);
+        $("div.eq_knowledge").each((i, elem) => {
+          const lore = $(elem).find("div.desc > span").text();
+          const dirtyDescription = $(elem).find("div.desc > p.tooltip").text();
+          const description = dirtyDescription
+            .trim() // Remove leading and trailing whitespace
+            .replace(/\s\s+/g, " "); // Turn 2 or more whitespace into 1 whitespace
+          loreDescriptions[lore] = description;
+        });
+        resolve();
+      }
+    );
+  });
+}
+
+const promises = [];
+const loreDescriptions = {};
+CLANS.forEach((clan) => {
+  const promise = requestWrapper(clan, loreDescriptions);
+  promises.push(promise);
+});
+Promise.all(promises).then(() => {
+  fs.writeFileSync(
+    `../descriptions/lores.json`,
+    JSON.stringify(loreDescriptions)
   );
 });
